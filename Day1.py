51a1a6260040ce2b05aa0769178a54eaac031d2c
shitalmahajan11 <68534159+shitalmahajan11@users.noreply.github.com>
2020-07-21 10:45:05 +0530

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,20 @@
+
+  1. What is Python ? what are the ways to install python in your PC ?
+      Ans:-   Python is an interpreted, object-oriented, high-level programming language. Python's simple, easy to learn syntax emphasizes readability and therefore reduces the cost of program maintenance.
+                 Python supports modules and packages, which encourages program modularity and code reuse.
+
+    installation steps:-
+       1.   Visit Anaconda.com
+       2.   go to individual Edition.
+       3.   Go to download option and select windows python 3.7 version.
+       4.   Download the python 3.7 version.
+       5.   At the beginning of the install, you need to click Next to confirm the installation.
+       6.   Then agree to the license.
+       7.   At the Advanced Installation Options screen,check second option and click install button.
+
+    2.   what is Code Environments ? type?
+    Ans:-      A code environment is a standalone and self-contained environment to run Python or R code. Each code environment has its own set of packages.
+                 3 main type of Code Environments:-
+                         1.text Editors
+                         2. Full IDEs
+                         3. Notebook Environment
