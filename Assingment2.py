f0dc1b170106af4425abb693e6f0b197feb41aab
shitalmahajan11 <68534159+shitalmahajan11@users.noreply.github.com>
2020-07-22 10:44:57 +0530

    Create Assingment2.py

---
+++ Assingment2.py
@@ -0,0 +1,79 @@
+
+# LetsUpgrade Assignment 2
+1. Back slash:- it is continuation sign.
+Ex. print*("hello \
+            welcome")
+           
+2. triple Quotes:-represent the strings containing both single and double quotes to eliminate the need of escaping any.
+Ex:- print("""this is python session""")
+
+3.String inside the quotes:-there are 2 way to declared on string inside the quotes.
+Ex.  1)  print('hello world')
+     2)   print("python's World")
+   
+4. Escape Sequence of String:- the backslash "\" is a special character, also called the "escape" character.and"\t" is a tab, "\n" is a newline, and "\r" is a carriage return are used.
+ Ex:- print("hello\tworld")
+      print("welcome\nhome")
+     
+5.Formatted output:- There are several ways to present the output of a program, data can be printed in a human-readable form, or written to a file for future use.
+Ex:- name=ABC
+     age=24
+     print("the name of person is", name," age is",age)
+     
+ # variable:- variables means linking of the data to name or A Python variable is a reserved memory location to store values.
+ Ex:- a=20
+ 
+#  Rules to variable
+1.  A variable name must start with a letter or the underscore character.
+2.  A variable name cannot start with a number.
+3.  A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+4.  Variable names are case-sensitive (age, Age and AGE are three different variables)
+
+# python Operator:-
+there are 7 type of operator
+1.  Arithmatic operator
+2.  Comparison Operator
+3.  Assignment Operator
+4.  Bitwise Operator
+5.  Logical operator
+6.  Membership Operator
+7.  Identity Operator
+
+# Arithmatic Operator:- Arithmetic operators are used with numeric values to perform common mathematical operations.
+   1.    + Addition
+   2.    - Subtraction
+   3.    * Multiplication
+   4.    / Division
+   5.    % Modulus
+   6.    ** Exponentiation
+   7.    // Floor division
+   
+   # Comparision Operator:-Comparison operators are used to compare two values.
+   1.    == Equal
+   2.    != Not equal
+   3.    > Greater than
+   4.    < Less than
+   5.    >= Greater than or equal to
+   6.    <= Less than or equal to
+   
+   #  Assignment Operator:-Assignment operators are used to assign values to variables.used = operator.
+   #  Bitwise operators are used to compare (binary) numbers:-
+   1.   & AND
+   2.  | OR
+   3.   ^ XOR
+   4.  ~ NOT
+   5.  << Zero fill left shift
+   6.  >> Signed right shift
+   
+  # Logical operator:-Logical operators are used to combine conditional statement.
+  1.  and
+  2.  or
+  3.  not
+ 
+  # Membership Operator:-Membership operators are used to test if a sequence is presented in an object.
+  1.  in
+  2. not in
+ 
+  #  Identity operators are used to compare the objects, not if they are equal, but if they are actually the same object, with the same memory locationIdentity Operator.
+  1.  is
+  2.  is not
